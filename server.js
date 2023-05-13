dec615ea3b3acb3bbcb2d856e04644ab13ceb96d
Anastasiya Kormilitsina <anastasiya.kormilitsina@gmail.com>
2023-05-14 02:08:06 +0500

    adds mongoose connection

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 const app = require("./app");
+const mongoose = require("mongoose");
+const DB_HOST =
+  "mongodb+srv://anastasiia:yBAnjpsWsrj1etst@cluster0.amaego9.mongodb.net/Contacts_reader?retryWrites=true&w=majority";
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log("Database connection successful");
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
